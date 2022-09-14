525328680aac037cc04ec618c465ec443289878d
vmoreno86 <victor.love86@gmail.com>
2022-09-15 00:44:03 +0200

    Se añade render de productos productCards

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
-
+const cardContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toogleDesktopMenu);
 menuHamIcon.addEventListener('click',toogleMobileMenu);
@@ -45,3 +45,80 @@ function toogleCarritoAside(){
 
     aside.classList.toggle('inactive');
 }
+
+const productsList = [];
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    //image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image: 'https://cdn.pixabay.com/photo/2013/07/13/13/43/racing-bicycle-161449_960_720.png'
+});
+
+productsList.push({
+    name: 'Laptop',
+    price: 1050,
+    image: 'https://static9.depositphotos.com/1682899/1155/i/600/depositphotos_11557809-stock-photo-laptop.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productsList.push({
+    name: 'Patinete',
+    price: 80,
+    image: 'https://st3.depositphotos.com/3332767/13005/i/600/depositphotos_130057840-stock-photo-blue-scooter-isolated.jpg'
+});
+
+
+
+function renderProducts(arr){
+    
+// <!-- <div class="product-card">
+// <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+// <div class="product-info">
+//   <div>
+//     <p>$120,00</p>
+//     <p>Bike</p>
+//   </div>
+//   <figure>
+//     <img src="./icons/bt_add_to_cart.svg" alt="">
+//   </figure>
+// </div>
+// </div> -->
+    for (product of arr) {
+    
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src',product.image);
+        //product es oigual a {name,price,image}
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardContainer.appendChild(productCard);
+    
+    }
+}
+
+renderProducts(productsList);
+
